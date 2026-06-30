643a481a775bef12559aedb49b06f1da0fe59a2a
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2026-06-30 12:18:59 -0600

    feat: add navigation bar structure and styles

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import "../styles/home.css"
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+    return (
+        < >
+            <Navbar />
+        </>
+    );
+}
