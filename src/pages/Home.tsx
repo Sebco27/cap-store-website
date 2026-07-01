bf2250bf556891f52753b95346b3e2cf6fd001f7
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2026-06-30 18:53:12 -0600

    feat: implement config for github pages deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import "../styles/home.css"
 import Navbar from "../components/Navbar";
 import Products from "../components/Products.tsx";
-import Footer from "../components/Footer.tsx";
-import Promo from "../components/Promo";
 
 export default function Home() {
     return (
