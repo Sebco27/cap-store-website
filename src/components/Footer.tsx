a19951cb4f04f5b5d638a39b639b64e6630b82d7
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2026-07-20 14:52:38 -0600

    feat: add site footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import "../styles/footer.css"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2026 Luminous Studios. Todos los derechos reservados.</p>
+            <p>
+                Sitio web desarrollado por{" "}
+                <a
+                    href="https://github.com/Sebco27"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    @Sebco27
+                </a>
+            </p>
+        </footer>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import "../styles/home.css"
 import Navbar from "../components/Navbar";
 import Products from "../components/Products.tsx";
+import Footer from "../components/Footer.tsx";
 
 export default function Home() {
     return (
         < >
             <Navbar />
             <Products />
+            <Footer />
         </>
     );
 }
